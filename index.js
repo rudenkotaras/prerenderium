0e615d3bb0b02ee6a9d097889f2bbe5635b360db
Vano Devium <webdevium@gmail.com>
2021-09-01 16:40:17 +0300

    wip

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const app = require('./lib/app');
+const app = require("./lib/app");
 
 app.listen(3000);
 
-process.on('unhandledRejection', (reason, p) => {
+process.on("unhandledRejection", (reason, p) => {
+  stopBrowser();
+  console.error("Unhandled Rejection at:", p, "reason:", reason);
+  process.exit(1);
+});
+
+process.on("SIGINT", function () {
+  stopBrowser();
+  setTimeout(() => {
+    process.exit(1);
+  }, 500);
+});
+
+function stopBrowser() {
   try {
     (async () => app.context.renderer.getBrowser().close())();
   } catch (e) {
-    console.error('Problem with browser stopping...');
+    console.error("Problem with browser stopping...");
   }
-  console.error('Unhandled Rejection at:', p, 'reason:', reason)
-  process.exit(1)
-});
+}
